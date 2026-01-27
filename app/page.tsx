da488589b45e5b05829b3e13eb531950480621f9
Diorphil <diorphilippo@gmail.com>
2026-01-27 14:45:41 -0700

    bug fixed for week3

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,18 @@
-<<<<<<< HEAD
 import Link from "next/link";
 export default function Home() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <div className=" flex justify-center items-start">
-      <Link className="p-10 bg-gray-600 text-white rounded-lg" href="/week-2">
+        <Link className="p-10 bg-gray-600 text-white rounded-lg" href="/week-2">
           <button type="button">week-2</button></Link>
-        </div>
+      </div>
       <br />
       <div className=" flex justify-center items-start">
-      <Link  href="/week-3">
-        <button className=" m-5 p-10 bg-gray-600 text-white rounded-lg" type="button">week-3</button></Link>
+        <Link href="/week-3">
+          <button className=" m-5 p-10 bg-gray-600 text-white rounded-lg" type="button">week-3</button></Link>
       </div>
-      </main>
-=======
-import Image from "next/image";
-import Link from "next/link";
-export default function Home() {
-  return(
-  <main>
-    <h1>CPRG 306: Web Development 2 - Assignments</h1>
-    <Link href="/week-2">
-    <button type="button">Week-2</button>
-    </Link>
-      </main>
-
->>>>>>> b334256fe73ede432e21e438f04270fa53bb9fa0
+    </main>
   );
+
 }
