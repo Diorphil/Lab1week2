b6929cd17ccbb6ad9ac369c91507276f365f788a
Diorphil <diorphilippo@gmail.com>
2026-01-27 14:35:10 -0700

    add week-3 shopping list

--- app/week-2/page.tsx
+++ app/week-2/page.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link'
 import StudentInfo from "./student-info";
 export default function Week2Page() {
  return (
 <div>
 <h1>Week 2</h1>
-<StudentInfo />
-</div>
+     <StudentInfo />
+     <Link href="/">Home page</Link>
+   </div>
  );
 }
